cd9a2236aebd940277fb3c05f3c9ce3132844616
Albert Arvidsson <albert.arvidsson@gmail.com>
2016-10-11 01:43:01 +0200

    Update CLI to hook up to Client and Navigator
    The options (nickname, host, port, token, navigator) can be passed like
    ./mazedrivers_bot --nickname=Albert. But they all have Defaults defined
    here.

--- lib/mazedrivers_bot/cli.ex
+++ lib/mazedrivers_bot/cli.ex
@@ -2,6 +2,11 @@ defmodule MazedriversBot.CLI do
   @moduledoc """
   CLI for usage via escript
   """
+  alias MazedriversBot.Client
+  alias MazedriversBot.Navigator
+
+  @default_ws_host "localhost"
+  @default_ws_port 8001
 
   def main(args) do
     args |> parse_args |> process
@@ -9,6 +14,12 @@ defmodule MazedriversBot.CLI do
 
   def process(options) do
     options
+    |> validate_navigator
+    |> set_default_host
+    |> set_default_port
+    |> set_default_nickname
+    |> set_default_token
+    |> Client.start
   end
 
   defp parse_args(args) do
@@ -22,5 +33,33 @@ defmodule MazedriversBot.CLI do
       ]
     )
     options
+  end
+
+  def validate_navigator(options) do
+    default_navigator = Navigator.random
+    navigator =
+      Keyword.get(options, :navigator) || Atom.to_string(default_navigator)
+    unless Navigator.validate(navigator) do
+      raise ArgumentError, message: "Invalid navigator #{navigator}"
+    end
+    Keyword.update(options, :navigator, default_navigator, &String.to_atom(&1))
+  end
+
+  defp set_default_host(options) do
+    Keyword.update(options, :host, @default_ws_host, &(&1))
+  end
+
+  defp set_default_port(options) do
+    Keyword.update(options, :port, @default_ws_port, &(&1))
+  end
+
+  defp set_default_nickname(options) do
+    navigator = Keyword.get(options, :navigator)
+    default_nickname = "#{navigator}#{:os.timestamp |> elem(2)}"
+    Keyword.update(options, :nickname, default_nickname, &(&1))
+  end
+
+  defp set_default_token(options) do
+    Keyword.update(options, :port, nil, &(&1))
   end
 end
